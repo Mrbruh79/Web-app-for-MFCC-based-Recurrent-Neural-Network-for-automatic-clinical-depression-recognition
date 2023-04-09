be4976c03e51ec25d06b0ba9e0baff410092853f
Arnav Bansal <arnavb1562002@gmail.com>
2023-04-09 20:15:41 +0530

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,20 @@ import { useState, useRef } from "react";
 import AudioRecorder from "./components/AudioRecorder";
 import Navigation from "./components/Navigation";
 import HomePage from "./components/HomePage";
-
+import Slid from "./components/Slid";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import AudioUpload from "./components/audioUpload";
 const App = () => {
       return (
-        <div className=" flex flex-col items-stretch h-screen">
-       {/* <Navigation />
+        <div className=" flex flex-col  h-screen">
+        <Navigation />
       
-       <HomePage /> */}
-       
+       {/* <HomePage />  */}
+       <h2 className="text-center text-white font-Roboto font-bold">Please upload your answers as an audio file </h2>
+       <Slid />
        <AudioRecorder/>
+       {/* <AudioUpload /> */}
        </div>
        
        
